348d87a14dfc06fcb11110b4dc58af03e6bb4b0a
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2025-01-04 14:55:33 +0700

    add footer

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
         <main className='space-y-[70px] pb-[100px]'>
           <Outlet />
         </main>
+        <Footer />
     </>
   )
 }
