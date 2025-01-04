e1d2398e4f2d3fd0b487304ef8d8de8620fe85b5
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2025-01-04 14:46:58 +0700

    add product list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Categories from '../components/Categories'
+import ProductList from '../components/ProductList'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Categories />
+      <ProductList />
     </>
   )
 }
